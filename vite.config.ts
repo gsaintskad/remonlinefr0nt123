f5df85fad0ecac6c081d3cf299e576f4ed94456a
iwouldneverto <glebsmile3303@gmail.com>
2025-02-16 13:02:49 +0100

    intv

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['localhost',"fe05-83-25-239-178.ngrok-free.app",'8cee-83-25-239-178.ngrok-free.app', '127.0.0.1','60cc-83-25-239-178.ngrok-free.app','5458-83-25-239-178.ngrok-free.app'],
+    allowedHosts: ['localhost', '127.0.0.1','60cc-83-25-239-178.ngrok-free.app','5458-83-25-239-178.ngrok-free.app'],
   }
 })
+//
